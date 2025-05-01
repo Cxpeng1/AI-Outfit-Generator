971af156a3aeeec0717ae192b7a9491b9eaabf49
Cxpeng1 <cxpeng0718@gmail.com>
2025-05-01 14:28:25 +0800

    Setting up and uploaded frontend

--- outfitfyp/settings.py
+++ outfitfyp/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'outfitfyp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "main" / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
